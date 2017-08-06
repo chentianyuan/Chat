d9e9ec9d2ffe02dad42b303f27de690312293514
chentianyuan <chentianyuan2017@outlook.com>
2017-08-06 21:39:02 +0800

    增加用户进入输入唯一昵称交互

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var io = require('socket.io')(http);
 //express对象实例使用get方法发送一个标签
 app.get("/",function(req,res){
 	//res.send('<h1>Hello socket.io</h1>')
+	//将整个页面传至服务器
 	res.sendfile('index.html');
 });
 
@@ -19,12 +20,17 @@ io.on('connection',function(socket){
 		//后端在此处只要检测登陆用户名的唯一性就可以了，其他的都可以交给前端来做
 	});
 	socket.on('disconnect',function(){
+		//disconnect为socket自带的事件
 	});
 	socket.on('chat message',function(msg){
 		//前端触发了聊天事件，后端对传来的数据进行处理
 		//io.emit给每个客户端发送事件
 		//前端触发聊天事件，因为需要广播到所有的socket，广播的操作前端做不到，只能由后端来做，这里的io.emit就是广播方法的一种
 		io.emit('chat message',msg);
+	});
+	socket.on('someoneEnter',function(msg){
+		console.log('有人进来了，昵称是' + msg);
+		io.emit('someoneEnter',msg);
 	});
 });
 
